21f113e8f7955c263e826a31eaa78b493a384aa7
Kevin Lee <wonseok.lee80@gmail.com>
2025-04-22 19:26:03 -0700

    add hosting

---
+++ web/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ textAlign:'center', marginTop:'3rem', fontFamily:'system-ui' }}>
+      <h1>Hello AllyWorld!</h1>
+      <p>This is a React SPA served by Firebase Hosting.</p>
+    </main>
+  );
+}
